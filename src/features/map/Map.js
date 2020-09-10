92e9ed9ccdb6181ff3ddc18eb5f939488788c747
Vincent Vachon <vincent@passenger1.com>
2020-09-10 14:13:11 -0400

    Starting setup

---
+++ src/features/map/Map.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import styles from './Map.scss';
+
+export default function Map() {
+
+    return (
+        <div>
+            Map
+        </div>
+    );
+}
